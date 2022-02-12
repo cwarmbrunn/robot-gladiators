cd853e0ba87a0845d6c65b23c8ba9587423ade20
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-02-12 13:09:18 -0500

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,10 @@
+// Game States:
+
+// "WIN" - Player robot has defeated all enemy-robots
+//  * Fight all enemy-robots
+//  * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
@@ -6,12 +13,13 @@ var playerMoney = 10;
 // You can also log multiple values at once like this 
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+
 // fight function 
-var fight = function() {
+var fight = function(enemyName) {
 
 //Alert players that they are starting the round
 window.alert("Welcome to Robot Gladiators!");
@@ -24,7 +32,7 @@ if (promptFight == "fight" || promptFight === "FIGHT") {
 //remove enemy's health by subtracting the amount set in the playerAttack variable
 enemyHealth = enemyHealth - playerAttack;
 console.log(
-    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    playerName + " attacked " +  + ". " + enemyName + " now has " + enemyHealth + " health remaining."
 );
 
 // check enemy's health
@@ -71,5 +79,6 @@ window.alert("You need to pick a valid option. Try again!")
 };
 
 // run fight function to start game
-fight();
-
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
